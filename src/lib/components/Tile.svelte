fac3a9338b4ca177c021296650955a856bf937d3
Matthew Cedric Calaycay <matthewc.calaycay@gmail.com>
2025-07-03 17:15:49 +0800

    feat: add Tile component with basic functionality

---
+++ src/lib/components/Tile.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { type TileProps } from '$lib/types';
+	import clsx from 'clsx';
+
+	let { isMine = false, isRevealed = false, mineCount = 0, onclick }: TileProps = $props();
+</script>
+
+<button
+	type="button"
+	aria-label="Tile"
+	{onclick}
+	class="h-[32px] w-[32px] bg-blue-600 text-white"
+>
+	{#if isRevealed}
+		{mineCount > 0 ? mineCount : ''}
+	{/if}
+</button>
