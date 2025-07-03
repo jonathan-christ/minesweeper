de4d71eb14c94477e31c5369a699e83773946ee2
Matthew Cedric Calaycay <matthewc.calaycay@gmail.com>
2025-07-03 19:38:24 +0800

    feat: integrate Game component into main page layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import Game from '$lib/components/Game.svelte';
+</script>
+
+<div class="bg-black flex flex-col items-center justify-center h-screen">
+	<Game />
+</div>
