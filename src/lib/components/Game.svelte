eccd392e0632fea25cdc25992832beb4fcb969d5
Matthew Cedric Calaycay <matthewc.calaycay@gmail.com>
2025-07-04 00:39:08 +0800

    feat: update game difficulty to hard and add reset button functionality

--- src/lib/components/Game.svelte
+++ src/lib/components/Game.svelte
@@ -3,12 +3,16 @@
 	import { GameController } from '$lib/game';
 	import type { TileProps } from '$lib/types';
 
-	const game = new GameController('easy');
+	const game = new GameController('hard');
 	const width = game.getWidth();
     
-    let tiles: TileProps[][] = [];
+    let tiles: TileProps[][] = $state([]);
 	game.tiles.subscribe(v => tiles = v);
 </script>
+
+<div id="controls" class="flex justify-center items-center gap-2">
+	<button class="bg-white p-4" onclick={() => game.resetGame()}>Reset</button>
+</div>
 
 <div
 	class="grid w-fit gap-0 divide-red-600 border-red-600 border-2 divide-x-2 divide-y-2"
