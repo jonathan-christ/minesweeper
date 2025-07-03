fecf0a2d6bdb5a55152869dba14c45205e90610b
Matthew Cedric Calaycay <matthewc.calaycay@gmail.com>
2025-07-03 19:39:38 +0800

    feat: enhance Tile component with context menu support and flagging functionality

--- src/lib/components/Tile.svelte
+++ src/lib/components/Tile.svelte
@@ -1,17 +1,37 @@
 <script lang="ts">
 	import { type TileProps } from '$lib/types';
-	import clsx from 'clsx';
 
-	let { isMine = false, isRevealed = false, mineCount = 0, onclick }: TileProps = $props();
+	let {
+		isMine = $bindable(false),
+		isRevealed = $bindable(false),
+		mineCount = $bindable(0),
+		isFlagged = $bindable(false),
+		onclick,
+		oncontextmenu
+	}: TileProps = $props();
+
+	function handleContextMenu(event: MouseEvent) {
+		event.preventDefault();
+		oncontextmenu?.();
+	}
 </script>
 
 <button
 	type="button"
 	aria-label="Tile"
 	{onclick}
-	class="h-[32px] w-[32px] bg-blue-600 text-white"
+	oncontextmenu={handleContextMenu}
+	class="h-[32px] w-[32px] bg-gray-400 text-white"
 >
 	{#if isRevealed}
-		{mineCount > 0 ? mineCount : ''}
+		{#if isMine}
+			<div class="h-full w-full bg-red-600">💣</div>
+		{:else}
+			<div class="h-full w-full bg-black">
+				{mineCount > 0 ? mineCount : ''}
+			</div>
+		{/if}
+	{:else if isFlagged}
+		<div class="h-full w-full bg-green-600">🚩</div>
 	{/if}
 </button>
