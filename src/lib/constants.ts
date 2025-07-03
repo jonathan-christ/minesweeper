430d369475bdb098c9c2014c0099c2760eccdb44
Matthew Cedric Calaycay <matthewc.calaycay@gmail.com>
2025-07-03 19:37:43 +0800

    feat: add difficulty constants for game setup

---
+++ src/lib/constants.ts
@@ -0,0 +1,7 @@
+import type { Difficulty } from "./types";
+
+export const DIFFICULTY_SETUP: Record<Difficulty, {width: number, height: number, mines: number}> = {
+    easy: {width: 9, height: 9, mines: 10},
+    medium: {width: 16, height: 16, mines: 40},
+    hard: {width: 16, height: 30, mines: 99}
+}
