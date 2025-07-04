62db2403aacc7e3b0281ce7e7a55ff08b5cd717e
Matthew Cedric Calaycay <matthewc.calaycay@gmail.com>
2025-07-04 21:44:53 +0800

    refactor: improve tile layout and styling in Game component
    - encapsulated tile rendering within styled divs for better structure
    - applied conditional class for the first row tiles to enhance visual effects
    - organized class management using a dedicated classes object for clarity

--- src/lib/components/Game.svelte
+++ src/lib/components/Game.svelte
@@ -12,23 +12,30 @@
 		});
 		return unsubscribe;
 	});
+
+	const classes = {
+		outer:
+			'bg-minecraft-button border-minecraft-border flex flex-col items-center justify-center border-4',
+		inner:
+			'bg-minecraft-button border-minecraft-border-inner border-b-minecraft-border-bottom grid w-fit gap-0 border-4'
+	};
 </script>
 
 <div id="controls" class="flex items-center justify-center gap-2">
 	<button class="bg-white p-4" onclick={() => game.resetGame()}>Reset</button>
 </div>
 
-<div
-	class="grid w-fit gap-0 divide-x-2 divide-y-2 divide-red-600 border-2 border-red-600"
-	style="grid-template-columns: repeat({width}, minmax(0, 1fr));"
->
-	{#each tiles as row, y (`row-${y}`)}
-		{#each row as cell, x (`cell-${y}-${x}`)}
-			<Tile
-				{...cell}
-				onclick={() => game.revealTile(x, y)}
-				oncontextmenu={() => game.flagTile(x, y)}
-			/>
+<div class={classes.outer}>
+	<div class={classes.inner} style="grid-template-columns: repeat({width}, minmax(0, 1fr));">
+		{#each tiles as row, y (`row-${y}`)}
+			{#each row as cell, x (`cell-${y}-${x}`)}
+				<Tile
+					{...cell}
+					class={y === 0 ? 'shadow-[inset_4px_8px_8px_-8px_rgba(0,0,0,1)]' : ''}
+					onclick={() => game.revealTile(x, y)}
+					oncontextmenu={() => game.flagTile(x, y)}
+				/>
+			{/each}
 		{/each}
-	{/each}
+	</div>
 </div>
