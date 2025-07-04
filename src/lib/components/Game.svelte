cfe3c6ded7f93586ee2bf6e2e17146e6d743b467
Matthew Cedric Calaycay <matthewc.calaycay@gmail.com>
2025-07-04 23:20:33 +0800

    refactor: improve layout and styling of Game component
    - integrated tailwind-merge for dynamic class management
    - adjusted outer and inner class definitions to enhance layout consistency
    - modified control button positioning for better alignment within the game interface

--- src/lib/components/Game.svelte
+++ src/lib/components/Game.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import Tile from './Tile.svelte';
 	import { GameController } from '$lib/game';
+	import { twMerge } from 'tailwind-merge';
 
 	const game = new GameController('hard');
 	const width = game.getWidth();
@@ -15,27 +16,31 @@
 
 	const classes = {
 		outer:
-			'bg-minecraft-button border-minecraft-border flex flex-col items-center justify-center border-4',
+			'shrink-0 bg-minecraft-button border-minecraft-border flex flex-col items-center justify-center border-4',
 		inner:
-			'bg-minecraft-button border-minecraft-border-inner border-b-minecraft-border-bottom grid w-fit gap-0 border-4'
+			'shrink-0 bg-minecraft-button border-minecraft-border-inner border-b-minecraft-border-bottom grid w-fit gap-0 border-4'
 	};
 </script>
 
-<div id="controls" class="flex items-center justify-center gap-2">
-	<button class="bg-white p-4" onclick={() => game.resetGame()}>Reset</button>
-</div>
+<div class="flex flex-col items-center justify-center w-fit">
+	<div class={twMerge(classes.outer, 'mb-4 w-full')}>
+		<div id="controls" class={twMerge(classes.inner, 'flex items-center justify-start gap-2 w-full')}>
+			<button class="bg-white p-4" onclick={() => game.resetGame()}>Reset</button>
+		</div>
+	</div>
 
-<div class={classes.outer}>
-	<div class={classes.inner} style="grid-template-columns: repeat({width}, minmax(0, 1fr));">
-		{#each tiles as row, y (`row-${y}`)}
-			{#each row as cell, x (`cell-${y}-${x}`)}
-				<Tile
-					{...cell}
-					class={y === 0 ? 'shadow-[inset_4px_8px_8px_-8px_rgba(0,0,0,1)]' : ''}
-					onclick={() => game.revealTile(x, y)}
-					oncontextmenu={() => game.flagTile(x, y)}
-				/>
+	<div class={classes.outer + ' w-auto overflow-auto'}>
+		<div class={classes.inner + ' w-auto'} style="grid-template-columns: repeat({width}, minmax(0, 1fr));">
+			{#each tiles as row, y (`row-${y}`)}
+				{#each row as cell, x (`cell-${y}-${x}`)}
+					<Tile
+						{...cell}
+						class={y === 0 ? 'shadow-[inset_4px_8px_8px_-8px_rgba(0,0,0,1)]' : ''}
+						onclick={() => game.revealTile(x, y)}
+						oncontextmenu={() => game.flagTile(x, y)}
+					/>
+				{/each}
 			{/each}
-		{/each}
+		</div>
 	</div>
 </div>
