100f66197d39303f12b63dbbb9f9c7a776783c75
Matthew Cedric Calaycay <matthewc.calaycay@gmail.com>
2025-07-05 17:22:48 +0800

    feat: add Credits component to display audio sources
    - introduced a new Credits component to showcase audio sources used in the game
    - integrated the Credits component into the main page layout for easy access
    - added functionality to toggle the visibility of credits for improved user experience

---
+++ src/lib/components/Credits.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { Separator } from '$lib/components/ui/separator';
+	import Button from '$lib/components/Button.svelte';
+	let showCredits = $state(false);
+</script>
+
+<Button label="Credits" className="text-xs" onclick={() => (showCredits = !showCredits)} />
+{#if showCredits}
+	<div class="flex flex-col gap-1 text-left text-xs text-white/75 bg-black p-4">
+		<div><b>Dig sounds</b> - "gravel walking.aif" by tigersound</div>
+		<div><b>Button click</b> - "btn121.wav" by junggle</div>
+		<div><b>TNT explosion</b> - "largex.wav" by inferno</div>
+		<div><b>Torch place</b> - "Hitting Wood" by altfuture</div>
+	</div>
+{/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
 	import Game from '$lib/components/Game.svelte';
 	import CloudBackground from '$lib/components/CloudBackground.svelte';
+	import Credits from '$lib/components/Credits.svelte';
 </script>
 
 <CloudBackground />
-<div class="flex flex-col items-center justify-center md:h-screen md:w-screen w-[1024px] h-[768px]">
+<div class="relative flex h-[1024px] w-[1024px] flex-col items-center justify-center md:h-screen md:w-screen">
 	<Game />
+	<div class="absolute bottom-0">
+		<Credits />
+	</div>
 </div>
